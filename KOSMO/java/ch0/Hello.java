017464221b81d40f8b172e3ed0873168034d71b1
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-04-10 21:49:02 +0900

    Add files via upload

---
+++ KOSMO/java/ch0/Hello.java
@@ -0,0 +1,12 @@
+//Hello.java 만들기
+//콘솔에 Hello Java 문자열 출력하기
+
+public class Hello{
+	public static void main(String args[]){
+		System.out.println("Hello Java");
+	}
+
+}
+
+
+
---
+++ KOSMO/java/ch0/HelloJava.java
@@ -0,0 +1,39 @@
+// HelloJava.java 프로그램 만들기 
+// HelloJava 프로그램은 콘솔에 "안녕 자바" 문자열을 출력하기 프로그램이다.
+
+// HelloJava.java 프로그램은 C:\00.KOSMO108\10.JExam 폴더에 저장한다.
+
+public class HelloJava {
+
+	public static void main(String args[]){
+	
+		System.out.println("안녕 자바");
+		
+	}
+}
+
+/*
+1. HelloJava 프로그램을 편집기에서 만든다.
+2. HelloJava 프로그램을 .java 확장자로 저장한다.
+3. HelloJava 프로그램을 컴파일 하기 위해서 
+	HelloJava.java 프로그램이 있는 폴더를 찾아간다.
+	C:\00.KOSMO108\10.JExam\HelloJava.java
+	콘솔창에서 해당 디렉토리까지 찾아간다.
+
+	javac 명령어로 컴파일 한다. 
+	C:\00.KOSMO108\10.JExam>javac HelloJava.java
+
+	컴파일이 잘 되면 
+	C:\00.KOSMO108\10.JExam>dir HelloJava.class 
+4. HelloJava.class 화일을 java 명령어로 실행을 한다.
+	C:\00.KOSMO108\10.JExam>java HelloJava
+	안녕 자바
+*/
+
+
+
+
+
+
+
+
