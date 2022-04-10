750dafdd3914fc95f13f764a7cf0f1d4956cf256
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-04-10 22:13:24 +0900

    Add files via upload

---
+++ YYS/web/firstWeb/src/a/b/c/t6/servie/T6Service.java
@@ -0,0 +1,20 @@
+package a.b.c.t6.servie;
+
+import java.util.ArrayList;
+
+import a.b.c.t6.vo.T6VO;
+
+public interface T6Service {
+	
+	public ArrayList<T6VO> t6SelectAll();
+	public ArrayList<T6VO> t6SelectName(T6VO tvo);
+	public ArrayList<T6VO> t6SelectDate(T6VO tvo);
+
+	public boolean t6Insert(T6VO tvo);
+	public boolean t6Update(T6VO tvo);
+	public boolean t6Delete(T6VO tvo);
+	
+	// 회원번호 검색 추가 
+	public ArrayList<T6VO> t6SelectNum(T6VO tvo);
+
+}
---
+++ YYS/web/firstWeb/src/a/b/c/test/TestClass.java
@@ -0,0 +1,10 @@
+package a.b.c.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Test !!");
+	}
+
+}
