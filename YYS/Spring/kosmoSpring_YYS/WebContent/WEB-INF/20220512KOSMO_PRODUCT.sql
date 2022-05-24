be5d7684af74d6836d6e8d6a55c2196736af8301
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-05-24 18:28:16 +0900

    Add files via upload

--- YYS/Spring/kosmoSpring_YYS/WebContent/WEB-INF/20220512KOSMO_PRODUCT.sql
+++ YYS/Spring/kosmoSpring_YYS/WebContent/WEB-INF/20220512KOSMO_PRODUCT.sql
@@ -160,12 +160,50 @@ WHERE MDELETEYN='Y'
 )
 WHERE PAGESIZE=PAGESIZE;
 
+-- SELECT
+-- SELECT ALL
+SELECT
+        A.KMNUM  AS  KMNUM
+        , A.KMID AS KMID
+        , A.KMPW AS KMPW
+        , A.KMNAME AS KMNAMEAS
+        , A.KMHP AS KMHP
+         , A.KMEMAIL AS KMEMAIL
+         ,  A.KMZONE AS KMZONE
+         , A.KMROAD AS KMROAD
+         , A.KMROAD2 AS KMROAD2
+         ,A.KMJIBUN AS KMJIBUN
+         , A.KMIMAGE AS KMIMAGE
+         , A.MDELETEYN AS MDELETEYN
+         , TO_CHAR(TO_DATE(A.MINSERTDATE), 'YYYY-MM-DD') AS MINSERTDATE
+         , TO_CHAR(TO_DATE(A.MUPDATEDATE), 'YYYY-MM-DD') AS MUPDATEDATE
+FROM KOSMO_MEMBER A
+WHERE MDELETEYN='Y';
+
+select*from KOSMO_MEMBER;
+commit;
+
+-- login check
+SELECT 
+        A.KMID AS KMID
+        ,A.KMPW AS KMPW
+FROM KOSMO_MEMBER A
+WHERE A.MDELETEYN='Y'
+AND A.KMID='kimStar_ID'
+AND A.KMPW='123456';
+
+update kosmo_member
+set kmid='umeume', kmemail='umeume@naver.com'
+where kmnum='KM202205200004';
+
+commit;
 
 /*       ####################  장바구니 테이블   ####################     */
 DROP TABLE KOSMO_CART;
 CREATE TABLE KOSMO_CART(
     MCNUM VARCHAR2(20) PRIMARY KEY --장바구니번호
     ,KMNUM VARCHAR2(20)                              -- 회원번호
+    ,MNUM  VARCHAR2(20)                                -- 상품번호
     ,MID  VARCHAR2(20)                                     --상품아이디
     ,MCNT  NUMBER                                 --수량
     ,MNAME  VARCHAR2(300)                           --상품명
@@ -179,5 +217,78 @@ CREATE TABLE KOSMO_CART(
     ,MDELETEYN  VARCHAR(1) NOT NULL --등록여부
 );
 
+SELECT*FROM KOSMO_CART;
 
+SELECT NVL(MAX(SUBSTR(A.MCNUM, -4)), 0) + 1 AS KMNUM FROM KOSMO_CART  A;
+
+INSERT INTO KOSMO_CART(MCNUM, KMNUM,  MID, MCNT, MNAME, MPRICE
+                            , MPRICESUM,  MDES, MCOM, MIMG
+                            ,MINSERTDATE,MUPDATEDATE, MDELETEYN)
+VALUES (
+'1', '1',  '1', '1', '1', '1'
+ , '1',  '1', '1', '1'
+,SYSDATE,SYSDATE, 'Y'
+);
+
+ROLLBACK;
+
+-- SELECT ALL
+
+SELECT*FROM(
+SELECT
+    A.MCNUM                       AS    MCNUM                --장바구니번호
+    ,A.KMNUM                      AS    KMNUM                             -- 회원번호
+    ,A.MNUM					  AS  MNUM                  -- 상품번호
+    ,A.MID                               AS       MID                --상품아이디
+    ,A.MCNT                          AS   MCNT             --수량
+    ,A.MNAME                      AS     MNAME     --상품명
+     ,A.MPRICE                      AS  MPRICE        --가격
+    ,A.MPRICESUM              AS   MPRICESUM               -- 가격 누계액
+    ,A.MDES                           AS  MDES   --상품설명
+    ,A.MCOM                         AS MCOM   --상품제조사
+    ,A.MIMG                            AS MIMG   -- 이미지파일 (경로 혹은 이미지 이름)
+     , A.MDELETEYN AS MDELETEYN
+    , TO_CHAR(TO_DATE(A.MINSERTDATE), 'YYYY-MM-DD') AS MINSERTDATE
+    , TO_CHAR(TO_DATE(A.MUPDATEDATE), 'YYYY-MM-DD') AS MUPDATEDATE
+    ,CEIL(ROW_NUMBER() OVER(ORDER BY A.KMNUM)/ #{pageSize} ) AS pageSize	
+    ,COUNT( A.MCNUM) OVER() AS totalCount 
+FROM KOSMO_CART A
+WHERE A.MDELETEYN='Y'
+)
+WHERE PAGESIZE=#{curPage};
+
+SELECT*FROM KOSMO_CART;
+
+UPDATE KOSMO_CART
+SET MDELETEYN='Y'
+WHERE KMNUM='GUEST'
+AND MDELETEYN='N';
+
+ROLLBACK;
+
+COMMIT;
+
+-- ################ 게시판 ###################
+
+-- 게시판
+CREATE TABLE KOSMO_BOARD(
+     KBNUM VARCHAR2(20) PRIMARY KEY 
+    ,KBSUBJECT VARCHAR2(200)
+    ,KBNAME VARCHAR2(100)
+    ,KBCONTENT VARCHAR2(2000) 
+    ,KBFILE VARCHAR2(200) 
+    ,KBPW VARCHAR2(300)
+   ,MDELETEYN VARCHAR2(1) NOT NULL -- 등록여부
+   ,MINSERTDATE DATE NOT NULL --등록일
+   ,MUPDATEDATE DATE NOT NULL   --수정일
+);
+
+SELECT*FROM KOSMO_BOARD;
+
+SELECT A.TABLE_NAME, A.INDEX_NAME
+FROM ALL_IND_COLUMNS A;
+
+SELECT  /*+ INDEX_DESC(A SYS_C0013161) */
+        NVL(MAX(SUBSTR(A.KBNUM, -4)), 0) + 1  KBNUM 
+FROM    KOSMO_BOARD A;
 
